29da2a12195fa8ab7ba097f0063a05590f1b9217
Çağatay ERDOĞDU <cagatayerdogdu@hotmail.com>
2024-03-09 23:10:33 +0300

    HomeController: uygulamaTipiVieBag, PortfoyDetay: Çeşitli denemeler sonrası TODO eklenmesi, LayoutPage: Url.Content eklemeleri

--- CagatayErdogduCv/Controllers/HomeController.cs
+++ CagatayErdogduCv/Controllers/HomeController.cs
@@ -22,25 +22,34 @@ namespace CagatayErdogduCv.Controllers
             if (id != null || !id.IsEmpty())
             {
                 string gelenDeger = id;
-                string uygulamaTipi = gelenDeger.Substring(gelenDeger.IndexOf("_") +1 , 3);
+                string uygulamaTipi = gelenDeger.Substring(gelenDeger.IndexOf("_") + 1, 3);
                 string imgIsim = gelenDeger.Substring(0, gelenDeger.IndexOf("_")) + ".jpg";
                 string tipDizin = "";
                 switch (uygulamaTipi)
                 {
-                    case "app": tipDizin = "software";
+                    case "app":
+                        tipDizin = "software";
                         break;
-                    case "etl": tipDizin = "etl";
+                    case "etl":
+                        tipDizin = "etl";
                         break;
-                    case "sql": tipDizin = "sqlQuery";
+                    case "sql":
+                        tipDizin = "sqlQuery";
                         break;
-                    case "web": tipDizin = "web";
+                    case "web":
+                        tipDizin = "web";
                         break;
                 }
-                ViewBag.imgUrl = "~/assets/img/portfolio/" + tipDizin + "/" + imgIsim;
+                string urlImg = "/assets/img/portfolio/" + tipDizin + "/" + imgIsim;
+                ViewBag.imgUrl = urlImg;
                 ViewBag.detailValue = imgIsim;
+                ViewBag.uygulamaTipi = uygulamaTipi;
+                return View();
             }
+            else
+                return Redirect("Index");
+            
                         
-            return View();
 
         }
     }
