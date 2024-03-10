11ed20964f8b999b281818078a3c7fc68dfe0b63
Çağatay ERDOĞDU <cagatayerdogdu@hotmail.com>
2024-03-10 23:24:24 +0300

    LayoutPage2 oluşturuldu.

---
+++ CagatayErdogduCv/Controllers/PortfoyDetayController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CagatayErdogduCv.Controllers
+{
+    public class PortfoyDetayController : Controller
+    {
+        // GET: PortfoyDetay
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
